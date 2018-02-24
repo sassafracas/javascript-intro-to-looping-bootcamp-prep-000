fbfdf28066dc646b93403e2eee1540f8f942b46d
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-24 02:19:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop (array) {
-  for (var i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
   if (i < 1) {
     return `I am ${i} strange loops.`
   }
@@ -7,7 +7,7 @@ function forLoop (array) {
     return `I am ${i} strange loops.`
   }
   else {
-    array.push ["I am 1 strange loop"]
+    return "I am 1 strange loop."
   }
 }
 return array
